4d979ef81791be27e804b50ff10ed0503355d773
Sagar5426 <108585817+Sagar5426@users.noreply.github.com>
2024-08-31 13:58:07 +0530

    Part 2 completed

--- Expense Tracker -Sagar/ContentView.swift
+++ Expense Tracker -Sagar/ContentView.swift
@@ -10,31 +10,38 @@ import SwiftUI
 struct ContentView: View {
     // Intro Visibility Status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
+    // App Lock Properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled:Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     //Active tab
     @State private var activeTab: Tab = .settings
     
     var body: some View {
-        TabView(selection: $activeTab) {
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenAppGoesBackground) {
             
-            Search() //view displayed on selection
-                .tag(Tab.search) //id type something
-                .tabItem { Tab.search.tabContent } //creates view inside tab
             
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
-            
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
-        }
-        .tint(appTint)
-        .sheet(isPresented: $isFirstTime) {
-            IntroScreen()
-                .interactiveDismissDisabled()
+            TabView(selection: $activeTab) {
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
+                
+                Search() //view displayed on selection
+                    .tag(Tab.search) //id type something
+                    .tabItem { Tab.search.tabContent } //creates view inside tab
+                
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
+                
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .tint(appTint)
+            .sheet(isPresented: $isFirstTime) {
+                IntroScreen()
+                    .interactiveDismissDisabled()
+            }
         }
     }
 }
